62f67b584c1ceb1e19a6ee34c380f1e4e051bceb
Huệ Từ <tuluu.38@HT.local>
2023-01-05 20:45:10 +0700

    update data

--- js/header.js
+++ js/header.js
@@ -231,7 +231,7 @@ export function renderHeaderNav(){
 
         $(".search-logo-cart").innerHTML = 
         `
-            <div class="box-search-input-icon flex" style="width: 210px; height: 60px; margin-left: 10px">
+            <div class="box-search-input-icon flex" style="width: 70%; height: 50px; margin-left: 10px">
                 <button class="search-btn flex" style="width: fit-content ;background-color: white; margin:0px">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-search" viewBox="0 0 16 16">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
